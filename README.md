95fbfc05f5b2dc9e1f111d054bcbc4c4dbdd9c82
graymalkin <sjc80@kent.ac.uk>
2014-06-05 19:51:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+barcamp-tags
+============
+
+This is a dirty script to generate names for on name tags for barcamp. It's designed to work with AutoCAD DXF file.
+
+# Requires
+- python
+    - [dxfwrite 1.2.0](https://pypi.python.org/pypi/dxfwrite/1.2.0)
+	    `pip install dxfwrite`
+	    or
+	    ```
+	    wget https://pypi.python.org/packages/source/d/dxfwrite/dxfwrite-1.2.0.tar.gz#md5=56bd40206e6dbf4a3be2f3b6936a2c4f
+	    tar -xfv dxfwrite-1.2.0.tar.gz
+	    python dxfwrite-1.2.0/setup.py install
+	    ```
---
+++ barcamp-tags.py
@@ -0,0 +1,59 @@
+# I have no idea what I'm doing.
+import sys, getopt, csv
+from dxfwrite import DXFEngine as dxf
+
+offsetX = 7.08
+offsetY = 22.03
+
+# Pinched from http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+def main(argv):
+	datafile = ''
+	outputfile = ''
+	try:
+		opts, args = getopt.getopt(argv,"ho:d:",["ofile=","dfile="])
+	except getopt.GetoptError:
+		print 'barcamp-tags.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'barcamp-tags.py -i <inputfile> -o <outputfile>'
+			sys.exit()
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-d", "--dfile"):
+			datafile = arg
+
+	# Open the DXF file...
+	drawing = dxf.drawing(outputfile)
+
+	row = 1
+	column = 1
+	with open(datafile, 'rb') as csvfile:
+		data = csv.reader(csvfile, delimiter=',', quotechar='|')
+		# I don't understand python.
+		for x in data:
+			for item in x:
+				x = row * 85.6
+				y = column * 53.98
+
+				print item
+
+				# Append \n to long names
+				item = item.replace(' ', '\r\n', 1)
+
+				text = dxf.mtext(item, (x+offsetX, y+offsetY), height=8.5, rotation=0)
+				text.layer = 'TEXT'
+				text.color = 5 # Blue
+				drawing.add(text)
+
+				# Increment rows and columns (lim 6 rows)
+				row = row + 1
+				if row == 7:
+					row = 1
+					column = column + 1
+
+
+	drawing.saveas(outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
